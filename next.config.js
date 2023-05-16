613f1a947cda6663a0b73b56017b8de7c6a678a9
Dillon Laird <dillonalaird@gmail.com>
2023-05-15 22:34:00 -0700

    updated asset prefix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 // @ts-check
+const isProd = process.env.NODE_ENV === 'production'
 
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   output: 'export',
-  basePath: '/dillonalaird.github.io',
+  basePath: isProd ? '/dillonalaird.github.io' : '',
+  assetPrefix: isProd ? '/dillonalaird.github.io/' : '',
   images: {
     unoptimized: true,
   },
