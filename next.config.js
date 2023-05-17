30ae81996ac143a0d03e43389d68e237b479bfe7
Dillon Laird <dillonalaird@gmail.com>
2023-05-16 20:57:18 -0700

    try / path

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 // @ts-check
-const isProd = process.env.NODE_ENV === 'production'
 
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/dillonalaird.github.io' : '',
-  assetPrefix: isProd ? '/dillonalaird.github.io/' : '',
+  basePath: '/',
   images: {
     unoptimized: true,
   },
